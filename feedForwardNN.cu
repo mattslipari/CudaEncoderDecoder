623c83daa5e6fdf5846d0b6846b2d2565a18b1a8
wirehack <kevinwirehack@gmail.com>
2018-11-17 10:44:02 -0500

    init code

---
+++ feedForwardNN.cu
@@ -0,0 +1,13 @@
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <stdio.h>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+__global__ void feedForward(){
+
+}
